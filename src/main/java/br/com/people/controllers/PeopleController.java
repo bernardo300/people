f67987297654b416d5e74eb7645c3e520dac1155
Bernardo Lopes de Aguiar Filho <bernardolopes.luz@hotmail.com>
2022-10-09 13:23:50 -0300

    hello

---
+++ src/main/java/br/com/people/controllers/PeopleController.java
@@ -0,0 +1,13 @@
+package br.com.people.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class PeopleController {
+
+  @GetMapping("/")
+  public String hello(){
+    return "CI Heroku";
+  }
+}
